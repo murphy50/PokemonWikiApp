b3235fbdc7fd8cfcda16b8db499ef2902777f48b
murphy50 <80280986+murphy50@users.noreply.github.com>
2023-11-19 16:29:44 +0300

    init: PokemonWiki app

---
+++ PokemonWiki/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokemonWiki
+//
+//  Created by murphy on 17.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
